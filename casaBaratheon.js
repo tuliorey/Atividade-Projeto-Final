0a469a718e6deb4650fecef769fa54e1f02699ca
Túlio D'El-Rey <tuliorey@outlook.com>
2023-06-20 23:04:36 -0300

    inserção de imagens via API

--- casaBaratheon.js
+++ casaBaratheon.js
@@ -33,6 +33,27 @@ function mudarTexto(texto) {
     }
 }
 
-    
-  
-  
+var urls = [
+  { texto: "Robert Baratheon", urlId: '7', elementId: 'robertBaratheon' },
+  { texto: "Stannis Baratheon", urlId: '18', elementId: 'stannisBaratheon' }
+];
+
+document.addEventListener("DOMContentLoaded", function() {
+  for (var i = 0; i < urls.length; i++) {
+    var url = urls[i];
+    fetchCharacterData(url);
+  }
+});
+
+function fetchCharacterData(url) {
+  var urlAPI = 'https://thronesapi.com/api/v2/Characters/' + url.urlId;
+
+  fetch(urlAPI)
+    .then(response => response.json())
+    .then(response => exibir(response.imageUrl, url.elementId));
+}
+
+function exibir(imageUrl, elementId) {
+  var characterImage = document.getElementById(elementId);
+  characterImage.src = imageUrl;
+}
